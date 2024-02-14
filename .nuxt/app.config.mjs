7ef1ea0334769c02971806682494bccbec221390
RatRyan <ryantratajczak@gmail.com>
2024-02-13 19:08:07 -0700

    fix: removed node_modules

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,20 @@
+
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
+
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "3b921f09-4e63-47f8-8f4e-2883ad91f871"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
+
+
+
+export default /*@__PURE__*/ defuFn(inlineConfig)
