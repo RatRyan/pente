baf3d225d18818a2eddc61eb9d86c65508317023
RatRyan <ryantratajczak@gmail.com>
2024-02-13 19:33:08 -0700

    feat: added board unit test

--- tests/import.test.ts
+++ tests/import.test.ts
@@ -1,3 +1,6 @@
 describe('import vue components', () => {
-  test('normal imports as expected', async () => {});
+  test('normal imports as expected', async () => {
+    const cmp = await import('../components/Board.vue');
+    expect(cmp).toBeDefined();
+  });
 });
