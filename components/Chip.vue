a28b66ad009bf03bb756714c8555d5ad64c3f66e
EthanMorin <EthanMorin2004@gmail.com>
2024-02-13 19:27:50 -0700

    Create Chip.vue

---
+++ components/Chip.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :style="{ backgroundColor: color }" class="chip"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chip {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+</style>
