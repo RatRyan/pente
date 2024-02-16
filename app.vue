1eede34b80d1e3d95ae459dc99d76f1b165158a7
RatRyan <ryantratajczak@gmail.com>
2024-02-15 18:45:24 -0700

    feat: add board rendering

--- app.vue
+++ app.vue
@@ -1,9 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { setupBoard, gameBoard } = useGame();
+setupBoard(19);
+</script>
 
 <template>
   <div>
-    <label>Enter Board size:</label><br />
-    <input type="number" /><br />
-    <button>Start Game</button>
+    <div v-for="(row, rowIndex) in gameBoard" :key="'row-' + rowIndex">
+      <span
+        class="tile"
+        v-for="(tile, colIndex) in row"
+        :key="'tile-' + rowIndex + '-' + colIndex"
+      >
+        {{ tile }}
+      </span>
+    </div>
   </div>
 </template>
+
+<style scoped></style>
