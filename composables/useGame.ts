314e9eef0c4ffd0140db37e7983d93ce1ed3b996
paden <little.paden@gmail.com>
2024-02-20 13:20:27 -0700

    Ai move logic implemented

--- composables/useGame.ts
+++ composables/useGame.ts
@@ -1,11 +1,29 @@
 export function useGame() {
+  enum Tile {
+    Black,
+    White,
+    Empty,
+  }
+
   const boardSize = useState('boardSize', () => 0);
-  const board = useState('board', () => 0);
+  const gameBoard = useState<Tile[][]>('gameBoard', () => []);
   const isPlayerTurn = useState('isPlayerTurn', () => false);
+
+  function setupBoard(size: number) {
+    const newBoard: Tile[][] = [];
+    for (let i = 0; i < size; i++) {
+      newBoard[i] = [];
+      for (let j = 0; j < size; j++) {
+        newBoard[i][j] = Tile.Empty;
+      }
+    }
+    gameBoard.value = newBoard;
+  }
 
   return {
     boardSize,
-    board,
+    gameBoard,
     isPlayerTurn,
+    setupBoard,
   };
 }
