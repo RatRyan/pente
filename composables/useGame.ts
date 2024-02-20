ffe7669a28bffc06a1743afbbbd592d9c7057beb
RatRyan <ryantratajczak@gmail.com>
2024-02-20 16:52:47 -0700

    fix: restructure useGame

--- composables/useGame.ts
+++ composables/useGame.ts
@@ -10,14 +10,14 @@ export function useGame() {
     White = 'white.png',
   }
 
-  const gameBoard = useState<Tile[][]>('gameBoard', () => []);
+  const board = useState<Tile[][]>('gameBoard', () => []);
+  const boardSize = useState('boardSize', () => 19);
   const tileSize = useState('tileSize', () => '');
+  const playerCaptures = useState('playerCaptures', () => 0);
+  const computerCaptures = useState('computersCaptures', () => 0);
   const isPlayerTurn = useState('isPlayerTurn', () => false);
-  const gameStarted = useState('started', () => false);
-  let boardSize: number;
-  let playerCaptures = 0;
-  let aiCaptures = 0;
-  let winner;
+  const gameStarted = useState('gameStarted', () => false);
+  const winner = useState<'player' | 'computer' | null>('winner', () => null);
 
   function setupBoard(size: number) {
     const newBoard: Tile[][] = [];
@@ -27,106 +27,28 @@ export function useGame() {
         newBoard[i][j] = Tile.Empty;
       }
     }
-    gameBoard.value = newBoard;
+    board.value = newBoard;
     tileSize.value =
       Math.round((height.value - height.value * 0.3) / size) + 'px';
 
-    boardSize = size;
-    startingStone();
-  }
-
-  async function placeStone(col: number, row: number) {
-    let clickedTile = gameBoard.value[col][row];
-
-    await new Promise((r) => setTimeout(r, 10));
-    //Check if valid move, if aiMove call function again, if player move return
-    if (clickedTile != Tile.Empty && !isPlayerTurn.value) {
-      aiMove(col, row);
-    } else if (clickedTile != Tile.Empty && isPlayerTurn.value) {
-      return;
-    }
-
-    if (isPlayerTurn.value) {
-      clickedTile = Tile.Black;
-      gameBoard.value[col][row] = clickedTile;
-      isPlayerTurn.value = !isPlayerTurn.value;
-      aiMove(col, row);
-    } else {
-      clickedTile = Tile.White;
-      gameBoard.value[col][row] = clickedTile;
-      isPlayerTurn.value = !isPlayerTurn.value;
-    }
-  }
-
-  function startingStone() {
-    let center = boardSize / 2 - 0.5;
+    boardSize.value = size;
+    const center = size / 2 - 0.5;
     placeStone(center, center);
+    isPlayerTurn.value == true;
   }
 
-  function aiMove(col: number, row: number) {
-    if (getRandomBool()) {
-      if (getRandomBool()) {
-        if (getRandomBool()) {
-            placeStone(col + 1, row + 1);
-        } else {
-          placeStone(col + 1, row - 1);
-        }
-      } else {
-        if (getRandomBool()) {
-          placeStone(col - 1, row + 1);
-        } else {
-          placeStone(col - 1, row - 1);
-        }
-      }
-    } else {
-      if (getRandomBool()) {
-        if (getRandomBool()) {
-          placeStone(col, row + 1);
-        } else {
-          placeStone(col, row - 1);
-        }
-      } else {
-        if (getRandomBool()) {
-          placeStone(col - 1, row);
-        } else {
-          placeStone(col - 1, row);
-        }
-      }
-    }
-  }
+  function placeStone(col: number, row: number) {
+    let clickedTile = board.value[col][row];
+    if (clickedTile == Tile.Empty) return;
 
-  function checkWin(col: number, row: number) {
-    
-    if (playerCaptures == 5 || aiCaptures == 5) {
-      if(playerCaptures == 5) { 
-        winner = 'player'
-      } else {
-        winner = 'ai'
-      }
-    } else {
-      
-    }
-  }
-
-  function getRandomBool() {
-    return Math.random() > 0.5;
-  }
-
-  function getRandomInt() {
-    return Math.floor(Math.random() * (boardSize +  1));
-  }
-
-  function isEmptyTile(col: number, row: number) {
-    if (gameBoard.value[col][row] === Tile.Empty) {
-      return true
-    }
-    return false
+    board.value[col][row] = Tile.Black;
+    isPlayerTurn.value = false;
   }
 
   return {
     Tile,
     tileSize,
-    gameBoard,
+    board,
     gameStarted,
     setupBoard,
     placeStone,
