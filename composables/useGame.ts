6d98ba0ab7571965463aab1f610dd43675448c03
paden <little.paden@gmail.com>
2024-02-20 13:20:00 -0700

    Ai move logic implemented

--- composables/useGame.ts
+++ composables/useGame.ts
@@ -1,3 +1,5 @@
+import {useWindowSize} from "@vueuse/core";
+
 const { height } = useWindowSize();
 
 export function useGame() {
@@ -10,6 +12,7 @@ export function useGame() {
   const gameBoard = useState<Tile[][]>('gameBoard', () => []);
   const tileSize = useState('tileSize', () => '');
   const isPlayerTurn = useState('isPlayerTurn', () => false);
+  let boardSize: number;
 
   function setupBoard(size: number) {
     const newBoard: Tile[][] = [];
@@ -22,20 +25,73 @@ export function useGame() {
     gameBoard.value = newBoard;
     tileSize.value =
       Math.round((height.value - height.value * 0.3) / size) + 'px';
+
+    boardSize = size;
+    startingStone()
   }
 
-  function placeStone(col: number, row: number) {
+  async function placeStone(col: number, row: number) {
     let clickedTile = gameBoard.value[col][row];
-    if (clickedTile != Tile.Empty) return;
+
+    await new Promise(r => setTimeout(r, 10));
+    //Check if valid move, if aiMove call function again, if player move return
+    if (clickedTile != Tile.Empty && !isPlayerTurn.value) {
+      aiMove(col, row)
+    }else if(clickedTile != Tile.Empty && isPlayerTurn.value){
+      return
+    }
 
     if (isPlayerTurn.value) {
       clickedTile = Tile.Black;
+      gameBoard.value[col][row] = clickedTile;
+      isPlayerTurn.value = !isPlayerTurn.value;
+      aiMove(col, row)
     } else {
       clickedTile = Tile.White;
+      gameBoard.value[col][row] = clickedTile;
+      isPlayerTurn.value = !isPlayerTurn.value;
     }
+  }
 
-    gameBoard.value[col][row] = clickedTile;
-    isPlayerTurn.value = !isPlayerTurn.value;
+  function startingStone(){
+    let center = (boardSize / 2) - 0.5
+    placeStone(center, center)
+  }
+
+  function aiMove(col: number, row: number){
+    if(getRandomBool()){
+      if(getRandomBool()){
+        if(getRandomBool()){
+          placeStone(col + 1, row + 1)
+        }else{
+          placeStone(col + 1, row - 1)
+        }
+      }else{
+        if(getRandomBool()){
+          placeStone(col - 1, row + 1)
+        }else{
+          placeStone(col - 1, row - 1)
+        }
+      }
+    }else{
+      if(getRandomBool()){
+        if(getRandomBool()){
+          placeStone(col, row + 1)
+        }else{
+          placeStone(col, row - 1)
+        }
+      }else{
+        if(getRandomBool()){
+          placeStone(col - 1, row)
+        }else{
+          placeStone(col - 1, row)
+        }
+      }
+    }
+  }
+
+  function getRandomBool() {
+    return Math.random() > 0.5;
   }
 
   return {
