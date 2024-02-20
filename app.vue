85ff0a911ecc15b4de715d054791f11b57802488
RatRyan <ryantratajczak@gmail.com>
2024-02-19 23:45:38 -0700

    feat: add filler content until features are finished

--- app.vue
+++ app.vue
@@ -32,6 +32,22 @@ function handleStartGame() {
               <button @click="handleStartGame()">Start Game</button>
             </div> -->
           </div>
+          <div v-else class="game-status">
+            <h2>
+              this is the area where we would display player/bot info like how
+              many captures each player has
+            </h2>
+            <p>
+              AS MY BODY BEGINS TO DECAY ROSES ENTWINE MY BONES WHERE I LAY I AM
+              THE ONE FROM THE EARTH TAKE MY HAND AND I'LL SHOW YOU MY SACRED
+              HOME STAND ALL ALONE IN THE LIGHT CLOSE YOUR EYES AND I'LL GUIDE
+              YOU INTO THE DARKNESS FOREST CREATURES STAY CLOSE TO THEIR KIN
+              CLEANSING WATERS ERASE ALL MY SINS I AM THE ONE FROM THE EARTH
+              TAKE MY HAND AND I'LL SHOW YOU MY SACRED HOME STAND ALL ALONE IN
+              THE LIGHT CLOSE YOUR EYES AND I'LL GUIDE YOU INTO THE DARKNESS 
+            </p>
+            <h2>Also, throw the save game button down here :P</h2>
+          </div>
         </div>
       </div>
     </div>
