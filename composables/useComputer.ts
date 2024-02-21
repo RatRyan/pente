f401bccb17337d1380e445280945c924986b1138
RatRyan <ryantratajczak@gmail.com>
2024-02-20 17:14:33 -0700

    feat: add computer turn

--- composables/useComputer.ts
+++ composables/useComputer.ts
@@ -1,5 +1,17 @@
 export function useComputer() {
-  function computerMove(col: number, row: number) {}
+  const { board, boardSize, Tile } = useGame();
+
+  function computerMove() {
+    while (true) {
+      const row = Math.floor(Math.random() * boardSize.value);
+      const col = Math.floor(Math.random() * boardSize.value);
+
+      if (board.value[col][row] == Tile.Empty) {
+        board.value[col][row] == Tile.White;
+        break;
+      }
+    }
+  }
 
   return {
     computerMove,
