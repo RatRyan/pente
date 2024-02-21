ad6e83bee55895de4fa72cc5ceabe6fd85b73e06
RatRyan <ryantratajczak@gmail.com>
2024-02-21 00:49:24 -0700

    fix: fix capturing

--- composables/useGame.ts
+++ composables/useGame.ts
@@ -26,10 +26,10 @@ export function useGame() {
         newBoard[i][j] = Tile.Empty;
       }
     }
-    board.value = newBoard;
+
     tileSize.value =
       Math.round((height.value - height.value * 0.3) / size) + 'px';
-
+    board.value = newBoard;
     boardSize.value = size;
     const center = size / 2 - 0.5;
     board.value[center][center] = Tile.White;
@@ -56,6 +56,14 @@ export function useGame() {
     checkForWin();
   }
 
+  function isWithinBounds(x: number, y: number): boolean {
+    return x >= 0 && x < boardSize.value && y >= 0 && y < boardSize.value;
+  }
+
+  function isOppositeColor(tile: Tile, stoneColor: Tile): boolean {
+    return tile === (stoneColor === Tile.Black ? Tile.White : Tile.Black);
+  }
+
   function checkLineWin(col: number, row: number) {
     checkLine(col, row, 1, 0); // Horizontal
     checkLine(col, row, 0, 1); // Vertical
@@ -74,31 +82,51 @@ export function useGame() {
       const currentRow = row + i * yDir;
 
       // Checks for in-bounds and same stones are the same color
-      if (currentCol < 0 || currentCol >= boardSize.value) break;
-      if (currentRow < 0 || currentRow >= boardSize.value) break;
+      if (!isWithinBounds(currentCol, currentRow)) return;
       if (board.value[currentCol][currentRow] !== stoneColor) break;
 
       // If all checks pass, increment the counter
       count++;
     }
 
-    if (stoneColor === Tile.Black && count > 1) {
-      console.log(`Found ${count} in a line`);
-    }
     if (count === 5) {
       winner.value = stoneColor === Tile.Black ? 'player' : 'computer';
     }
   }
 
   function checkForCapture(col: number, row: number) {
-    checkCapture(col, row, 1, 0); // Right
-    checkCapture(col, row, -1, 0); // Left
-    checkCapture(col, row, 0, 1); // Down
-    checkCapture(col, row, 0, -1); // Up
+    // Nested for loop for checking cardinals + intercardinals
+    for (let y = -1; y < 2; y++) {
+      for (let x = -1; x < 2; x++) {
+        checkCapture(col, row, x, y);
+      }
+    }
   }
 
   function checkCapture(col: number, row: number, xDir: number, yDir: number) {
-    let stoneColor = board.value[col][row];
+    const stoneColor = board.value[col][row];
+    const oppositeColor = stoneColor === Tile.Black ? Tile.White : Tile.Black;
+
+    const firstX = col + 1 * xDir;
+    const firstY = row + 1 * yDir;
+    const secondX = col + 2 * xDir;
+    const secondY = row + 2 * yDir;
+
+    if (!isWithinBounds(firstX, firstY)) return;
+    if (!isWithinBounds(secondX, secondY)) return;
+
+    if (
+      board.value[firstX][firstY] == oppositeColor &&
+      board.value[secondX][secondY] == oppositeColor
+    ) {
+      if (board.value[col + 3 * xDir][row + 3 * yDir] == stoneColor) {
+        board.value[firstX][firstY] = Tile.Empty;
+        board.value[secondX][secondY] = Tile.Empty;
+        stoneColor == Tile.Black
+          ? (playerCaptures.value += 2)
+          : (computerCaptures.value += 2);
+      }
+    }
   }
 
   function checkForWin() {
@@ -112,11 +140,11 @@ export function useGame() {
     }
 
     // Check for a capture win
-    if (playerCaptures.value === 5) {
+    if (playerCaptures.value === 10) {
       winner.value = 'player';
       return;
     }
-    if (computerCaptures.value === 5) {
+    if (computerCaptures.value === 10) {
       winner.value = 'computer';
       return;
     }
